4ffefd436f06cdc84e4c5205f3cdcdafbe5c3876
sookraja <sookraja@sheridancollege.ca>
2023-01-24 15:25:04 -0500

    ICE 1 Version 2

--- Exercise1/Exercise1_New/src/exercise1/CardTrick.java
+++ Exercise1/Exercise1_New/src/exercise1/CardTrick.java
@@ -1,13 +1,15 @@
 package exercise1;
-
+ 
+import java.util.Scanner;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  * This is the Branch Code
  * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
- * modifier Annette 
+ * @author Paul Bonenfant Jan 25, 2022
+ * @author Annette Sookraj Jan 23, 2023
+ * 
  */
 public class CardTrick {
     
@@ -18,12 +20,22 @@ public class CardTrick {
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
             card.setValue((int)(1 + Math.random() * 13));
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            card.setSuit(Card.SUITS[(int)(0 + Math.random() * 3)]);
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
+            hand [i] = card;
+            System.out.println(hand[i].getValue() + " of " + hand[i].getSuit());
         }
-
+        
+        Card [] user = new Card [1];
+        Scanner k = new Scanner(System.in);
+        
+        System.out.print("Enter a card value: ");
+        
+        System.out.print("Enter a suit: ");
+        String suit = k.nextLine();
+        
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
